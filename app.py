4358882b642342ba034e96be77b6c7ab9b5c1afb
darthbeep <darthbeep@gmail.com>
2017-09-27 00:51:13 -0400

    that was a solid 2 hours of my life I I will never get back

--- app.py
+++ app.py
@@ -3,26 +3,29 @@
 #HW05 -- Jinja Tuning
 #2017-09-27
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import csv
+import random
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    retStr = "go to occupations"
-    return retStr
+	retStr = "<a href='" + url_for('occupy') + "'>go to occupations<a>"
+	return retStr
 
 @app.route("/occupations")
 def occupy():
-    #test array to show arrays can be turned into tables
-    #fakearr =[[1, 2, 3], [4, 5, 6]]
-    #same code as from reader.py
-    target_doc = csv.reader(open('occupations/occupations.csv', 'rU'), delimiter= ",", quotechar = "\"")
-    return render_template('data.html', foo='Census of Occupations', csv=target_doc)
-    return retStr
+	#test array to show arrays can be turned into tables
+	#fakearr =[[1, 2, 3], [4, 5, 6]]
+	#same code as from reader.py
+	target_doc = csv.reader(open('occupations/occupations.csv', 'rU'), delimiter= ",", quotechar = "\"")
+	target_doc2 = csv.reader(open('occupations/occupations.csv', 'rU'), delimiter= ",", quotechar = "\"")
+	r = random.random()*98.8 #Number slightly reduced due to jinja being weird
+	return render_template('data.html', title='Census of Occupations', csv=target_doc, rand=r, csv2=target_doc2)
+	return retStr
 
-                            
+
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+	app.debug = True
+	app.run()
