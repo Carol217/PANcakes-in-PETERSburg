82f26afbfd93048021de7eda81ed6c71eb4fbc79
Valery Chen <cpan3@stuy.edu>
2017-09-26 20:51:18 -0400

    can now print table

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
+#Carol Pan and Shaina Peters
+#SoftDev1 pd7
+#HW05 -- Jinja Tuning
+#2017-09-27
+
 from flask import Flask, render_template
 import csv
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "go to occupations"
+    retStr = "go to occupations"
+    return retStr
 
 @app.route("/occupations")
 def occupy():
-    fakearr =[[1, 2, 3], [4, 5, 6]]
-    target_doc = csv.reader(open('occupations.csv', rU), delimiter
-    return render_template('data.html', foo='title', csv=csv)
+    #test array to show arrays can be turned into tables
+    #fakearr =[[1, 2, 3], [4, 5, 6]]
+    #same code as from reader.py
+    target_doc = csv.reader(open('occupations/occupations.csv', 'rU'), delimiter= ",", quotechar = "\"")
+    return render_template('data.html', foo='Census of Occupations', csv=target_doc)
+    return retStr
+
+                            
 
 if __name__ == "__main__":
     app.debug = True
